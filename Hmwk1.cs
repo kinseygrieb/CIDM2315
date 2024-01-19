20ff42d0813733e1c46cccb036b7c8e8b0c544e3
kinseygrieb <156350166+kinseygrieb@users.noreply.github.com>
2024-01-19 10:18:25 -0600

    Add files via upload
    Homework1: part one

---
+++ Hmwk1.cs
@@ -0,0 +1,13 @@
+﻿namespace Homework1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //code for Homework1
+        float x = 2.5f;
+        float y = 3.3f;
+        float z = 4*x*x + 3*y;
+        Console.WriteLine("The value of Z is: " + z);
+    }
+}
